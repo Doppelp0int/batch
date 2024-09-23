87e8d3951024d03d8adbb414ac1c8e8e06366d42
Doppelp0int <148204656+Doppelp0int@users.noreply.github.com>
2024-09-23 17:52:52 +0200

    Update WSL_install.sh

--- WSL_install.sh
+++ WSL_install.sh
@@ -3,20 +3,19 @@
 sudo apt-get update && sudo apt-get upgrade -y
 
 # Docker installieren
-sudo apt-get install ca-certificates curl apt-transport-https -y
+sudo apt-get update
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.gpg
-sudo chmod 644 /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Docker Repository hinzufügen
+# Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin net-tools -y
-
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # Benutzer zur Docker-Gruppe hinzufügen
 sudo usermod -aG docker $USER
 
