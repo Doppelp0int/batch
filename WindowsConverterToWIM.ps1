98e8dbcf227a26fdb895d0cb31c1d9a12472d93a
Doppelp0int <148204656+Doppelp0int@users.noreply.github.com>
2024-09-23 16:36:19 +0200

    Update WindowsConverterToWIM.ps1

--- WindowsConverterToWIM.ps1
+++ WindowsConverterToWIM.ps1
@@ -1,21 +1,38 @@
-@echo off
-set "ISOPath=%userprofile%\Downloads\windows.iso"
-set "ExtractPath=%userprofile%\Downloads\Windows"
+#!/bin/bash
+# Benutzer erstellen und Passwort setzen
+echo "ubuntu:ubuntu" | sudo chpasswd
+sudo usermod -aG sudo ubuntu
 
-echo "Extrahiere Dateien aus der ISO: %ISOPath%"
+# System aktualisieren
+sudo apt update && sudo apt upgrade -y
 
-:: Erstelle Zielordner
-if not exist "%ExtractPath%" mkdir "%ExtractPath%"
+# Docker installieren
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y ca-certificates curl gnupg
 
-:: Extrahiere die ISO-Datei mit 7-Zip
-"C:\Program Files\7-Zip\7z.exe" x "%ISOPath%" -o"%ExtractPath%" -r -y
+# Docker GPG-Schlüssel hinzufügen
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc > /dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-echo "Extrahieren abgeschlossen. Die Dateien wurden in den Ordner %ExtractPath% extrahiert."
+# Docker-Repository hinzufügen
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo "Führe DISM-Befehle aus:"
-set "WIMPath=%userprofile%\Downloads\Windows\Sources\install.wim"
+# Docker installieren
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-dism /Get-WimInfo /WimFile:"%ExtractPath%\sources\install.esd"
-dism /export-image /SourceImageFile:"%ExtractPath%\sources\install.esd" /SourceIndex:5 /DestinationImageFile:"%WIMPath%" /Compress:max /CheckIntegrity
+# Docker-Post-Installation Schritte: Benutzer zur Docker-Gruppe hinzufügen
+sudo usermod -aG docker ubuntu
 
-echo "DISM-Befehle abgeschlossen."
+# Docker Compose installieren
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Portainer installieren
+sudo docker volume create portainer_data
+sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+
+# Neustart erforderlich für die Docker-Gruppenänderung
+echo "Docker und Portainer wurden installiert. Ein Neustart wird empfohlen."
