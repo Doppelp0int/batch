7a922503760aa8d15342ede186a30e6964662baf
Doppelp0int <148204656+Doppelp0int@users.noreply.github.com>
2024-09-24 15:18:26 +0200

    Update WSL_install.sh

--- WSL_install.sh
+++ WSL_install.sh
@@ -20,7 +20,9 @@ echo \
 
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin net-tools -y
-
+sleep 5
+service docker start
+sleep 5
 # Portainer Installation (kein sudo erforderlich, da du als root arbeitest)
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always \
